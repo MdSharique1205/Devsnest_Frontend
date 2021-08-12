4d38b7bde2363f583669534819171bcdf3bf5a2e
Sharique <mdsharique1205@gmail.com>
2021-08-13 03:21:08 +0530

    Completed day28

--- day28/src/App.js
+++ day28/src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [place, setPlace] = useState("Bhilai");
+  const [data, setData] = useState({});
+  const fetchAPI = () => {
+    fetch(
+      `http://api.weatherapi.com/v1/current.json?key=eb56638403994750a7f203334211108&q=${place}`
+    )
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 form">
+            <input
+              type="text"
+              value={place}
+              onChange={(e) => {
+                setPlace(e.target.value);
+              }}
+            />
+            <button
+              className="btn btn-primary"
+              onClick={() => {
+                fetchAPI();
+              }}
+            >
+              Submit{" "}
+            </button>
+          </div>
+          <div className="weather offset-md-4 col-12 col-md-4">
+            <div className="card">
+              {data.location ? (
+                <div>
+                  <img src={data.current.condition.icon} alt="" />
+                  <div className="temp">{data.current.temp_c}°</div>
+                  <div className="place">{data.location.name}</div>
+                  <div className="condition">{data.current.condition.text}</div>
+                  <div className="container more_details">
+                    <div className="row">
+                      <div className="col">
+                        <div className="title">Wind Speed</div>
+                        <div className="data">
+                          {data.current.wind_kph}
+                          <span className="unit">kmph</span>
+                        </div>
+                      </div>
+                      <div className="col">
+                        <div className="title">Humidity</div>
+                        <div className="data">
+                          {data.current.humidity}
+                          <span className="unit">%</span>
+                        </div>
+                      </div>
+                      <div className="col">
+                        <div className="title">Precipitation</div>
+                        <div className="data">
+                          {data.current.precip_mm}
+                          <span className="unit">mm</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <h2>Not Found</h2>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
